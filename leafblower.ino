7fb2b06eebeb7ee754d1725f6f680a95823305b8
Christoph Royer <mail@christophroyer.com>
2023-08-16 20:17:35 +0200

    Added input and Ramps between

--- leafblower.ino
+++ leafblower.ino
@@ -3,36 +3,50 @@
 #define EDF_LEFT 5
 #define EDF_RIGHT 6
 
-#define DELAY 1000
+#define POWER_PIN 8
+
 
 #define RAMP_TIME 1000
+#define LOOP_DELAY 100
 
-// max is 1000
 #define ZERO 1000
-#define POWER 1000
+// 0-1000
+#define LOW_POWER 0
+#define FULL_POWER 1000
 
 Servo edfLeft;
 Servo edfRight;
 Servo ledBuiltin;
 
-void setAll(int val) {
-  edfLeft.writeMicroseconds(val + ZERO);
-  edfRight.writeMicroseconds(val + ZERO);
-  ledBuiltin.writeMicroseconds(val + ZERO);
+int current_value = LOW_POWER;
+int target_value = LOW_POWER;
+
+void setAll() {
+  edfLeft.writeMicroseconds(current_value + ZERO);
+  edfRight.writeMicroseconds(current_value + ZERO);
+  ledBuiltin.writeMicroseconds(current_value + ZERO);
 }
 
 void setup() {
   edfLeft.attach(EDF_LEFT);
   edfRight.attach(EDF_RIGHT);
   ledBuiltin.attach(LED_BUILTIN);
-  setAll(0);
+  setAll();
 
-  while (millis() < DELAY) {}
-  while((millis() - DELAY) < RAMP_TIME) {
-    setAll((POWER * (millis() - DELAY)) / RAMP_TIME);
-  }
-  setAll(POWER);
+  pinMode(POWER_PIN, INPUT);
 }
 
 void loop() {
+  bool input = digitalRead(POWER_PIN);
+  target_value = input ? FULL_POWER : LOW_POWER;
+  if (current_value < target_value) {
+    current_value += 1000 * RAMP_TIME / LOOP_DELAY;
+    current_value = min(current_value, target_value);
+  } else if (current_value > target_value) {
+    current_value -= 1000 * RAMP_TIME / LOOP_DELAY;
+    current_value = max(current_value, target_value);
+  }
+  setAll();
+
+  delay(LOOP_DELAY);
 }
